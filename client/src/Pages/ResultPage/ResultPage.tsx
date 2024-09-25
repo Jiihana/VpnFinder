d1b3b2cda01d5f84e9f2c32d0026b3f15233a0c3
Eve Bouchard <eve.bouchard@septeo.com>
2024-09-26 00:42:14 +0200

    series only

--- client/src/Pages/ResultPage/ResultPage.tsx
+++ client/src/Pages/ResultPage/ResultPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, colors, FormControlLabel, FormGroup, Stack, Switch, Typography } from '@mui/material';
 import FilmSearchButton from '../Home/FilmSearchButton';
 import { useParams } from 'react-router-dom';
 import { AppHttpClient } from '../../HttpClient/AppHttpClient';
@@ -14,37 +14,54 @@ const ResultPage = () => {
     const dataContext = useContext(DataContext);
 
     const [films, setFilms] = useState<FilmResultModel[]>([]);
+    const [seriesOnly, setSeriesOnly] = useState(false);
 
-    console.log(`data context: ${dataContext.includeAdult}`);
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSeriesOnly(event.target.checked);
+    };
 
     useEffect(() => {
         const fetchFilmsAndTv = async () => {
-            try {
-                const resultMovies = await AppHttpClient.GetFilms(film as string, dataContext.includeAdult.toString() as string);
+            const films = seriesOnly ? [] : await fetchFilms();
+            const tvs = await fetchTvs();
 
-                if (!resultMovies.success) {
-                    console.error('Une erreur est survenue lors de la récupération des films, mais la request est passée');
-                    return;
-                }
-
-                const resultTv = await AppHttpClient.GetTvs(film as string, dataContext.includeAdult.toString() as string);
-
-                if (!resultTv.success) {
-                    console.error('Une erreur est survenue lors de la récupération des tv, mais la request est passée');
-                    return;
-                }
-
-                const films = resultMovies.value.films.filter(FilmHelper.hasEnoughDatasFilm);
-                const tvs = resultTv.value.tvs.filter(TvHelper.hasEnoughDatasTv).map(TvHelper.convertTvToFilm);
-
-                setFilms([...films, ...tvs]);
-            } catch (error) {
-                console.error('Une erreur est survenue lors de la récupération des films et tv', error);
-            }
+            setFilms([...films, ...tvs]);
         };
 
         fetchFilmsAndTv();
-    }, [film, dataContext.includeAdult]);
+    }, [film, dataContext.includeAdult, seriesOnly]);
+
+    const fetchFilms = async (): Promise<FilmResultModel[]> => {
+        try {
+            const resultFilms = await AppHttpClient.GetFilms(film as string, dataContext.includeAdult.toString() as string);
+
+            if (!resultFilms.success) {
+                console.error('Une erreur est survenue lors de la récupération des films, mais la requête est passée');
+                return [];
+            }
+
+            return resultFilms.value.films.filter(FilmHelper.hasEnoughDatasFilm) || [];
+        } catch (error) {
+            console.error('Une erreur est survenue lors de la récupération des films', error);
+            return [];
+        }
+    };
+
+    const fetchTvs = async (): Promise<FilmResultModel[]> => {
+        try {
+            const resultTv = await AppHttpClient.GetTvs(film as string, dataContext.includeAdult.toString() as string);
+
+            if (!resultTv.success) {
+                console.error('Une erreur est survenue lors de la récupération des tv, mais la request est passée');
+                return [];
+            }
+
+            return resultTv.value.tvs.filter(TvHelper.hasEnoughDatasTv).map(TvHelper.convertTvToFilm) || [];
+        } catch (error) {
+            console.error('Une erreur est survenue lors de la récupération des tv', error);
+            return [];
+        }
+    };
 
     return (
         <Stack
@@ -98,22 +115,52 @@ const ResultPage = () => {
             </Stack>
 
             <Stack
-                display="flex"
-                spacing={5}
                 sx={{
-                    width: '100%', // Ajustez la largeur pour qu'elle ne dépasse pas le conteneur
-                    maxWidth: '80%', // Définissez une largeur maximale pour éviter le débordement
+                    width: '100%',
+                    maxWidth: '100%',
                     height: 'auto',
                     alignItems: 'center',
                     justifyContent: 'center',
-                    overflow: 'hidden'
+                    display: 'flex'
                 }}
             >
-                {films.length === 0 ? (
-                    <Loader message={'No film/serie found, try a different name'} />
-                ) : (
-                    films.map((film, index) => <ResultFilmComponent key={index} film={film} />)
-                )}
+                <Box
+                    sx={{
+                        width: '80%',
+                        maxWidth: '100%',
+                        height: 'auto',
+                        alignItems: 'center',
+                        justifyContent: 'start',
+                        display: 'flex'
+                    }}
+                >
+                    <FormGroup sx={{ display: 'flex', width: '15%' }}>
+                        <FormControlLabel
+                            control={<Switch checked={seriesOnly} onChange={handleChange} />}
+                            label="Series only"
+                            labelPlacement="end"
+                            sx={{ paddingLeft: '5%' }}
+                        />
+                    </FormGroup>
+                </Box>
+                <Stack
+                    display="flex"
+                    spacing={5}
+                    sx={{
+                        width: '100%',
+                        maxWidth: '80%',
+                        height: 'auto',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        overflow: 'hidden'
+                    }}
+                >
+                    {films.length === 0 ? (
+                        <Loader message={'No film/serie found, try a different name'} />
+                    ) : (
+                        films.map((film, index) => <ResultFilmComponent key={index} film={film} />)
+                    )}
+                </Stack>
             </Stack>
         </Stack>
     );
